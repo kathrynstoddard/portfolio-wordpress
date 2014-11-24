0ba50d6fcb235d6776b579e7e013516b3fb324db
Kathryn Stoddard <kathryn.stoddard@gmail.com>
2014-11-24 14:16:44 -0800

    Cleaning up code in footer.php and page.php in order to begin adding content from html prototype

--- wp-content/themes/kathrynstoddard-child/footer.php
+++ wp-content/themes/kathrynstoddard-child/footer.php
@@ -12,9 +12,7 @@
 
   <footer id="colophon" class="site-footer" role="contentinfo">
     <div class="site-info">
-      <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'kathrynstoddard' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'kathrynstoddard' ), 'WordPress' ); ?></a>
-      <span class="sep"> | </span>
-      <?php printf( __( 'Theme: %1$s by %2$s.', 'kathrynstoddard' ), 'kathrynstoddard', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+
     </div><!-- .site-info -->
   </footer><!-- #colophon -->
 </div><!-- #page -->
